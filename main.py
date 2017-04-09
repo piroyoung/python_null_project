d6e03a30d7472f2f44d83007b39c464a5652cc2c
Hiroki Mizukami <mizukami_hiroki@cyberagent.co.jp>
2017-04-10 01:14:43 +0900

    implement logging and signal OChundling

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import pir.utils.functions
+import time
+from pir.utils.signal_handler import *
+from pir.utils.logger import *
 
 if __name__ == '__main__':
-    print(pir.utils.functions.get_hello())
+    while True:
+        LOGGER.info(SignalHandler.term)
+        time.sleep(1)
+        if SignalHandler.term is True:
+            LOGGER.info('Process has been terminated. signal id: {0}'.format(SignalHandler.num))
+            break
